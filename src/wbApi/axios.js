03f000d7713cb66883065d96e70c0db150cc4ee1
Rômullo S.A.S <romullo_sas@outlook.com>
2023-10-08 18:03:06 -0300

    heroku -> render ws 1

--- src/wbApi/axios.js
+++ src/wbApi/axios.js
@@ -1,7 +1,8 @@
 const axios = require('axios');
 
+const test = process.env.HEROKU_DB || 'http://localhost:3001/'
 // const test = "https://warbeasts-api.herokuapp.com";
-const test = 'http://localhost:3001/';
+// const test = 'http://localhost:3001/';
 
 const apiWB = axios.create({
     baseURL: test, // //process.env.NEXT_PUBLIC_CADASTRO_API_HOST
