9b230d232f917b4f64b484a9622fc1dee8c7b526
Rômullo S.A.S <romullo_sas@outlook.com>
2023-10-08 18:45:49 -0300

    render ws 4

--- src/wbApi/axios.js
+++ src/wbApi/axios.js
@@ -1,8 +1,10 @@
 const axios = require('axios');
 
 // const test = process.env.HEROKU_DB || 'http://localhost:3001/'
-const test = "https://warbeasts-api.herokuapp.com";
+// const test = "https://warbeasts-api.herokuapp.com";
 // const test = 'http://localhost:3001/';
+
+const test = process.env.WB_DB_MANAGER || 'http://localhost:3001/';
 
 const apiWB = axios.create({
     baseURL: test, // //process.env.NEXT_PUBLIC_CADASTRO_API_HOST
